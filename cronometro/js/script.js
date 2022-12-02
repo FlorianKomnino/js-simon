9834a63c170ec230775c273d1b2bc2edd043b50c
FlorianKomnino <florian.komnino@outlook.it>
2022-12-02 19:02:07 +0100

    added elements in html, added variables linked to element ids. Started working on functions to count till 100 second cents

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -1,2 +1,30 @@
 console.log('Hello World!');
 
+let centesimi = document.getElementById('centesimi');
+let secondi = document.getElementById('secondi');
+let minuti = document.getElementById('minuti');
+
+let avvia = document.getElementById('avvia');
+let pausa = document.getElementById('pausa');
+let azzera = document.getElementById('azzera');
+
+let cents = 0;
+let seconds = 0;
+let minutes = 0;
+
+avvia.addEventListener('click', myFunction(cents, centesimi))
+
+function myFunction(cheackVariable, element) {
+    let index = 1
+    for (index; index<100 ; index++) {
+        let clock = setInterval(numberChanger, 10, cheackVariable, element);
+        if (cheackVariable = 100){
+            clearInterval(clock);
+        }
+    }
+}
+
+function numberChanger(element, cheackVariable){
+    element.innerHTML = i;
+    cheackVariable++;
+}
