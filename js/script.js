1d2e49582db61c6eed6f582ca9790255f2555df7
FlorianKomnino <florian.komnino@outlook.it>
2022-12-02 17:56:29 +0100

    working button that counts to 10 clicks and becomes red at 10. Reset in 5 seconds

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 console.log('Hello World!');
 
+let button = document.getElementById('playButton');
+
+let counter = 0;
+
+let stopper;
+
+let gameOver = false;
+
+button.addEventListener ('click', function() {
+    if (!gameOver) {
+        counter++;
+        if ( counter < 10) {
+            button.innerHTML = `#${counter}`
+        } else if (counter == 10) {
+            button.innerHTML = `#${counter}`
+            button.style.backgroundColor = 'red';
+            setTimeout( function() {
+                counter = 0;
+                button.innerHTML = `#${counter}`
+                button.style.backgroundColor = '';
+            }, 5000)
+        }
+    }
+
+
+
+
+})
