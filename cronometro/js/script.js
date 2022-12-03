aaaf85d75e8e13451cc3f7284816315bd19045be
FlorianKomnino <florian.komnino@outlook.it>
2022-12-03 19:15:51 +0100

    added functions for each different size of time, linked to relative element, changed innerHTML of elements with setInterval

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -1,30 +1,45 @@
 console.log('Hello World!');
 
-let centesimi = document.getElementById('centesimi');
-let secondi = document.getElementById('secondi');
-let minuti = document.getElementById('minuti');
+let centsElement = document.getElementById('cents');
+let decSecElement = document.getElementById('decSec');
+let secondsElement = document.getElementById('seconds');
+let tenSecsElement = document.getElementById('tenSecs');
+let minutesElement = document.getElementById('minutes');
+let tenMinutesElement = document.getElementById('tenMinutes');
 
 let avvia = document.getElementById('avvia');
 let pausa = document.getElementById('pausa');
 let azzera = document.getElementById('azzera');
 
 let cents = 0;
+let decSec = 0;
 let seconds = 0;
+let tenSecs = 0;
 let minutes = 0;
+let tenMinutes = 0;
 
-avvia.addEventListener('click', myFunction(cents, centesimi))
 
-function myFunction(cheackVariable, element) {
-    let index = 1
-    for (index; index<100 ; index++) {
-        let clock = setInterval(numberChanger, 10, cheackVariable, element);
-        if (cheackVariable = 100){
-            clearInterval(clock);
+
+function getNumberScrolling(value, element, time) {
+    setInterval(() => {
+        if (value < 9) {
+            value++;
+            element.innerHTML = value
+        } else {
+            value = 0
+            element.innerHTML = value
         }
-    }
+    }, time);
 }
 
-function numberChanger(element, cheackVariable){
-    element.innerHTML = i;
-    cheackVariable++;
-}
+
+
+
+avvia.addEventListener("click", function () {
+    getNumberScrolling(cents, centsElement, 10);
+    getNumberScrolling(decSec, decSecElement, 100);
+    getNumberScrolling(seconds, secondsElement, 1000);
+    getNumberScrolling(tenSecs, tenSecsElement, 10000);
+    getNumberScrolling(tenSecs, minutesElement, 60000);
+    getNumberScrolling(tenSecs, tenMinutesElement, 600000);
+})
