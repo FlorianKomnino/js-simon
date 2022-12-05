a3327348279d1d5848242c1594c3d05e5a55a68a
FlorianKomnino <florian.komnino@outlook.it>
2022-12-05 19:31:08 +0100

    new approach to the problem without custom functions

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -18,40 +18,139 @@ let tenSecs = 0;
 let minutes = 0;
 let tenMinutes = 0;
 
+let timerWorking = false;
+let existingEventListenerPauseButton = false;
+let existingEventListenerResetButton = false;
 
 
-function getNumberScrollingTo10(value, element, time) {
-    setInterval(() => {
-        if (value < 9) {
-            value++;
-            element.innerHTML = value
-        } else {
-            value = 0
-            element.innerHTML = value
-        }
-    }, time);
-}
-
-function getNumberScrollingTo6(value, element, time) {
-    setInterval(() => {
-        if (value < 6) {
-            value++;
-            element.innerHTML = value
-        } else {
-            value = 0
-            element.innerHTML = value
-        }
-    }, time);
-}
-
+let centsTime;
+let decSecTime;
+let secondsTime;
+let tenSecsTime;
+let minutesTime;
+let tenMinutesTime;
 
 
 
 avvia.addEventListener("click", function () {
-    getNumberScrollingTo10(cents, centsElement, 10);
-    getNumberScrollingTo10(decSec, decSecElement, 100);
-    getNumberScrollingTo10(seconds, secondsElement, 1000);
-    getNumberScrollingTo6(tenSecs, tenSecsElement, 10000);
-    getNumberScrollingTo10(minutes, minutesElement, 60000);
-    getNumberScrollingTo6(tenSecs, tenMinutesElement, 600000);
+    if (!timerWorking) {
+
+        centsTime = setInterval(() => {
+            if (cents < 9) {
+                cents++;
+                centsElement.innerHTML = cents
+            } else {
+                cents = 0
+                centsElement.innerHTML = cents
+            }
+        }, 10);
+
+        decSecTime = setInterval(() => {
+            if (decSec < 9) {
+                decSec++;
+            } else if (decSec < 99) {
+                decSec++;
+                decSecElement.innerHTML = String(decSec)[0];
+            } else {
+                decSec = 0
+                decSecElement.innerHTML = String(decSec)[0];
+            }
+        }, 10);
+
+        secondsTime = setInterval(() => {
+            if (seconds < 99) {
+                seconds++;
+            } else if (seconds < 999) {
+                seconds++;
+                secondsElement.innerHTML = String(seconds)[0];
+            } else {
+                seconds = 0
+                secondsElement.innerHTML = String(seconds)[0];
+            }
+        }, 10);
+
+        tenSecsTime = setInterval(() => {
+            if (tenSecs < 999) {
+                tenSecs++;
+            } else if (tenSecs < 9999) {
+                tenSecs++;
+                tenSecsElement.innerHTML = String(tenSecs)[0];
+            } else {
+                tenSecs = 0
+                tenSecsElement.innerHTML = String(tenSecs)[0];
+            }
+        }, 10);
+
+        minutesTime = setInterval(() => {
+            if (minutes < 59999) {
+                minutes++;
+            } else if (minutes < 599999) {
+                minutes++;
+                minutesElement.innerHTML = String(minutes)[0];
+            } else {
+                minutes = 0
+                minutesElement.innerHTML = String(minutes)[0];
+            }
+        }, 10);
+
+        tenMinutesTime = setInterval(() => {
+            if (tenMinutes < 599999) {
+                tenMinutes++;
+            } else if (tenMinutes < 5999999) {
+                tenMinutes++;
+                tenMinutesElement.innerHTML = String(tenMinutes)[0];
+            } else {
+                tenMinutes = 0
+                tenMinutesElement.innerHTML = String(tenMinutes)[0];
+            }
+        }, 10);
+
+
+        if (!existingEventListenerPauseButton) {
+            pausa.addEventListener('click', function () {
+                console.log('Hello World!')
+                clearInterval(centsTime);
+                clearInterval(decSecTime);
+                clearInterval(secondsTime);
+                clearInterval(tenSecsTime);
+                clearInterval(minutesTime);
+                clearInterval(tenMinutesTime);
+
+                timerWorking = false;
+            })
+        }
+
+        if (!existingEventListenerResetButton) {
+
+            azzera.addEventListener('click', function () {
+                console.log('Hello World!')
+                clearInterval(centsTime);
+                clearInterval(decSecTime);
+                clearInterval(secondsTime);
+                clearInterval(tenSecsTime);
+                clearInterval(minutesTime);
+                clearInterval(tenMinutesTime);
+
+                cents = 0;
+                decSec = 0;
+                seconds = 0;
+                tenSecs = 0;
+                minutes = 0;
+                tenMinutes = 0;
+                tenMinutesElement.innerHTML = tenMinutes;
+                minutesElement.innerHTML = minutes;
+                tenSecsElement.innerHTML = tenSecs;
+                secondsElement.innerHTML = seconds;
+                decSecElement.innerHTML = decSec;
+                centsElement.innerHTML = cents;
+
+                timerWorking = false;
+            })
+        }
+
+        existingEventListenerPauseButton = true
+        existingEventListenerResetButton = true
+
+        timerWorking = true;
+    }
 })
